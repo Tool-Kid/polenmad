c8e4e140fbdb4daf2af4228fa04acc4949352792
Raúl Julián López Caña <rjlopezmad@gmail.com>
2022-02-13 15:23:35 +0100

    feat(polenmad): add demo for `polenmad-metric-card`

--- apps/polenmad/src/app/pages/metrics/metrics-page.module.ts
+++ apps/polenmad/src/app/pages/metrics/metrics-page.module.ts
@@ -3,9 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { MetricsPageRoutingModule } from './metrics-page-routing.module';
 import { MetricsLayoutComponent } from './layout/metrics-layout.component';
+import { MetricsModule } from '../../modules/metrics/metrics.module';
+
+const OWN_MODULES = [MetricsModule];
 
 @NgModule({
   declarations: [MetricsLayoutComponent],
-  imports: [CommonModule, MetricsPageRoutingModule],
+  imports: [CommonModule, ...OWN_MODULES, MetricsPageRoutingModule],
 })
 export class MetricsPageModule {}
