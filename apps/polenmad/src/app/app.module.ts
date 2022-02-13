3d820b10a1d0e593703800af3e02b2b39ffdc507
Raúl Julián López Caña <rjlopezmad@gmail.com>
2022-02-13 13:36:04 +0100

    feat(workspace): add `polenmad` app & configure `taiga-ui`

---
+++ apps/polenmad/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppComponent } from './app.component';
+import {
+  TuiDialogModule,
+  TuiNotificationsModule,
+  TuiRootModule,
+} from '@taiga-ui/core';
+import { AppRoutingModule } from './app-routing.module';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    TuiRootModule,
+    TuiNotificationsModule,
+    TuiDialogModule,
+    AppRoutingModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
