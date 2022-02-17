21f3033c31f5cf2998e905cd167be86b45cd1b05
Raúl Julián López Caña <rjlopezmad@gmail.com>
2022-02-17 08:57:49 +0100

    feat(api): add `EventEmittermodule`

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { EventEmitterModule } from '@nestjs/event-emitter';
+
 import { DYNAMO_DB_MODULE } from './dynamo-db';
+
 import { MetricsModule } from './metrics/metrics.module';
+import { PollenCollectorModule } from './pollen-collector/pollen-collector.module';
 
 @Module({
-  imports: [MetricsModule, DYNAMO_DB_MODULE],
+  imports: [
+    MetricsModule,
+    PollenCollectorModule,
+    DYNAMO_DB_MODULE,
+    EventEmitterModule.forRoot(),
+  ],
   controllers: [],
   providers: [],
 })
