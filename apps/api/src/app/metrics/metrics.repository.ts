1bff89b1d03b0a36dd8794e30a1d8cf237474c48
Raúl Julián López Caña <rjlopezmad@gmail.com>
2022-02-17 08:51:49 +0100

    feat(api): add `DynamoDBMetricsRepository`

--- apps/api/src/app/metrics/metrics.repository.ts
+++ apps/api/src/app/metrics/metrics.repository.ts
@@ -1,3 +1,8 @@
+import { PollenMetricsSchema } from './metrics.schema';
+
 export abstract class MetricsRepository {
-  abstract getMetrics(): Promise<any>;
+  abstract getMetrics(): Promise<PollenMetricsSchema[]>;
+  abstract updateMetrics(
+    metrics: PollenMetricsSchema
+  ): Promise<PollenMetricsSchema>;
 }
