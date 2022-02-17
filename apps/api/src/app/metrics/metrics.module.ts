ae93790877ce9e7522c27dca58737457a14a0766
Raúl Julián López Caña <rjlopezmad@gmail.com>
2022-02-17 09:15:17 +0100

    feat(api): use `DynamoDBMetricsRepository`

--- apps/api/src/app/metrics/metrics.module.ts
+++ apps/api/src/app/metrics/metrics.module.ts
@@ -2,12 +2,18 @@ import { Module } from '@nestjs/common';
 
 import { GetMetricsController } from './get/get-metrics.controller';
 import { MetricsRepository } from './metrics.repository';
-import { FooMetricsRepository } from './foo-metrics.repository';
 import { PollenCollectorModule } from '../pollen-collector/pollen-collector.module';
+import { OnPollenCollectorCompleted } from './on-pollen-collector-completed';
+import { DynamoDBMetricsRepository } from './dynamodb-metrics.repository';
+
+const EVENT_HANDLERS = [OnPollenCollectorCompleted];
 
 @Module({
   imports: [PollenCollectorModule],
   controllers: [GetMetricsController],
-  providers: [{ provide: MetricsRepository, useClass: FooMetricsRepository }],
+  providers: [
+    { provide: MetricsRepository, useClass: DynamoDBMetricsRepository },
+    ...EVENT_HANDLERS,
+  ],
 })
 export class MetricsModule {}
